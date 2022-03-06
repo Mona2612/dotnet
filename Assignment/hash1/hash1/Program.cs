4732956140cfea74cb904e479a33a2c2319cca4a
Mona2612 <100895097+Mona2612@users.noreply.github.com>
2022-03-06 17:16:10 +0530

    Add files via upload

---
+++ Assignment/hash1/hash1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace hash1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hash = new Hashtable();
+            hash.Add(1, "monika");
+            hash.Add(2, "senger");
+            hash.Add(3, "faridabad");
+            hash.Add(4, "haryana");
+            hash.Add(5, "student");
+            hash.Add(6, "dotnet");
+            hash.Add(7, "c++");
+            hash.Add(8, "c#");
+            hash.Add(9, "visual basic");
+            hash.Add(10, "datastructure");
+            foreach(DictionaryEntry a in hash)
+                Console.WriteLine("key:{0},value:{1}",a.Key,a.Value);
+        }
+    }
+}
