2540b903354b86bf8b0b1b37921bb6f520a12f40
Mona2612 <100895097+Mona2612@users.noreply.github.com>
2022-03-06 17:17:47 +0530

    Add files via upload

---
+++ Assignment/q1/q1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace q1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue que = new Queue();
+            que.Enqueue("monika");
+            que.Enqueue("senger");
+            que.Enqueue(26);
+            que.Enqueue(12);
+            que.Enqueue(2000);
+            Console.WriteLine("Number of Elements in Queue: {0}", que.Count);
+            Console.WriteLine("***Queue Elements***");
+            // Access Queue Elements
+            foreach (var item in que)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment/s1/s1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace s1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack stk = new Stack();
+            stk.Push("delhi");
+            stk.Push("faridabad");
+            stk.Push(2000);
+            stk.Push(199.23f);
+            stk.Push(null);
+            stk.Push("india");
+            Console.WriteLine("Number of Elements in Stack: {0}", stk.Count);
+            Console.WriteLine("***Stack Elements***");
+            // Access Stack Elements
+            foreach (var item in stk)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
