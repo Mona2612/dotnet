0084395b4cd4c71c71688499313dd8b418a19bd5
Mona2612 <100895097+Mona2612@users.noreply.github.com>
2022-03-07 13:49:17 +0530

    Add files via upload

---
+++ Assignment/sotedlist1/sotedlist1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace sotedlist1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList sl = new SortedList();
+            sl.Add("monika", "senger");
+            sl.Add("priyanka", "kumari");
+            sl.Add("rohit", "kothari");
+            sl.Add("tanvi", "singh");
+            sl.Add("yahsu", "bansal");
+            foreach (DictionaryEntry d in sl)
+            {
+                Console.WriteLine(d.Key + " " + d.Value);
+
+            }
+        }
+    }
+}
