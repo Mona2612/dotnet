aea71e74ac80330c5b4d37e71eece5790296a625
Mona2612 <100895097+Mona2612@users.noreply.github.com>
2022-03-10 20:20:47 +0530

    Add files via upload

---
+++ project/product/SQLQuery1.sql
@@ -0,0 +1,17 @@
+use product
+Create table Products
+(
+Id int primary key identity(1,1) not null,
+ProName nvarchar(50) not null,
+ProDesc nvarchar(100) not null,
+OnDate datetime default getdate()
+)
+select * from products
+create procedure sp_AddProducts
+(
+@ProName nvarchar(50), @Prodesc nvarchar(100)
+)
+as
+begin
+insert into Products (ProName,ProDesc) values (@ProName,@ProDesc)
+end
