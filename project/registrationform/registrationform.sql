1e278baa5e6dacc9c51dcb06ec40ae9c59561d90
Mona2612 <100895097+Mona2612@users.noreply.github.com>
2022-03-12 12:53:54 +0530

    Add files via upload

---
+++ project/registrationform/registrationform.sql
@@ -0,0 +1,25 @@
+use regestrationform
+create table regs_form
+(
+ID int primary key identity(1,1) not null,
+FirstName nvarchar(50) not null,
+LastName nvarchar(100) not null,
+Gender nvarchar(100) not null,
+Address nvarchar(100) not null,
+City nvarchar(100) not null,
+State nvarchar(100) not null,
+UserName nvarchar(100) not null,
+Email nvarchar(100) not null,
+Password nvarchar(100) not null
+)
+Select * from regs_form
+create procedure sp_regs
+(
+@FirstName nvarchar(50), @LastName nvarchar(100), @Gender nvarchar(100), @Address nvarchar(100), @City nvarchar(100), @State nvarchar(100),
+@UserName nvarchar(100), @Email nvarchar(100), @Password nvarchar(100)
+)
+as
+begin
+insert into regs_form(FirstName,LastName,Gender,Address,City,State,UserName,Email,Password) values (@FirstName,@LastName,@Gender,@Address,@City,@State,
+@UserName,@Email,@Password)
+end
