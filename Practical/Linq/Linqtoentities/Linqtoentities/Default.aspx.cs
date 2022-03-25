5a0192f1b8c45241a104f956c1027f46e2b103d6
Mona2612 <100895097+Mona2612@users.noreply.github.com>
2022-03-25 13:55:06 +0530

    Add files via upload

---
+++ Practical/Linq/Linqtoentities/Linqtoentities/Default.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Linqtoentities
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+
+            {
+
+                BindGridData();
+
+            }
+
+        }
+        private void BindGridData()
+
+        {
+
+            // Create the object of the Context defined in the constructor of the context class.  
+
+            db_employeeEntities db = new db_employeeEntities();
+
+            var result = from emp in db.EmployeeDetails
+
+                         select
+
+                         new
+
+                         {
+
+                             Id = emp.EmpId,
+
+                             Name = emp.EmpName,
+
+                             Location = emp.Location,
+
+                             Gender = emp.Gender
+
+                         };
+
+            gvDetails.DataSource = result.ToList();
+
+            gvDetails.DataBind();
+
+        }
+    }
+}
