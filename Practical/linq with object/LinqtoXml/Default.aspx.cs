2d57bfb939a34ac678a96d4535a2d8a41da493f6
Mona2612 <100895097+Mona2612@users.noreply.github.com>
2022-03-25 13:09:22 +0530

    Add files via upload

---
+++ Practical/linq with object/LinqtoXml/Default.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+
+namespace LinqtoXML
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+
+            {
+
+                XElement doc = XElement.Load(Server.MapPath("EmployeeDetails.xml"));
+
+                var result = from ed in doc.Descendants("employee")
+
+                             where Convert.ToInt32(ed.Element("salary").Value) >= 20000
+
+                             select new
+
+                             {
+
+                                 Id = ed.Element("empid").Value,
+
+                                 Name = ed.Element("empname").Value,
+
+                                 Salary = ed.Element("salary").Value,
+
+                                 Gender = ed.Element("gender").Value
+
+                             };
+
+                gvDetails.DataSource = result;
+
+                gvDetails.DataBind();
+            }
+        }
+    }
+}
+    
